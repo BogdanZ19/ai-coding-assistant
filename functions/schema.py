54b3eca588688338e9f70fff78e8cbfd3bc670cc
BogdanZ19 <bogdanzainia1@gmail.com>
2026-01-17 20:58:09 +0200

    complete schema with all callable functions

--- functions/schema.py
+++ functions/schema.py
@@ -1,6 +1,6 @@
 from google.genai import types
 
-get_files_info = types.FunctionDeclaration(
+schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
     description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
     parameters=types.Schema(
@@ -14,8 +14,65 @@ get_files_info = types.FunctionDeclaration(
     ),
 )
 
-
-available_functions = types.Tool(
-    function_declarations=[get_files_info]
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Returns the first 10,000 characters from the file. Notes if the file has been truncated to fit the 10k characters limit",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to get characters from, relative to the working directory.",
+            )
+        },
+        required=["file_path"],
+    ),
 )
 
+schemma_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Returns the STDOUT and STDERR after running the specified file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to run, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(type=types.Type.STRING),
+                description="Array of strings representing arguments to run the file with.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Write to selected file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to write to, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to be written in the file. The file will be overwritten to contain only the content from this parameter.",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schemma_run_python_file,
+        schema_write_file,
+    ]
+)
